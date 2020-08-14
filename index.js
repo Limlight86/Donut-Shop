2776e0eb264e1315e557cd7d1895e81e8e835459
Lazaro <lquin008@gmail.com>
2020-08-14 15:06:46 -0400

    POST vote routes and db queries added

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ app.get("/votes", async (_request, response) =>{
   response.json(result.rows)
 })
 
+app.post("/votes", async (request, response)=>{
+  const {voter, donut} = request.body
+  if (!voter) {
+    response.status(406).json({ error: 'You must be logged in to vote' });
+  }
+  else {
+    const voteToDelete = await db.query(`SELECT * FROM votes WHERE user=$1 AND date=CURRENT_DATE`, [voter])
+    if (voteToDelete.rows){
+      await db.query(`DELETE FROM votes WHERE voter=$1 AND date=CURRENT_DATE RETURNING *`, [voter])
+    }
+    const result = await db.query(`INSERT INTO votes (donut, voter) VALUES ($1, $2) RETURNING *`, [donut, voter])
+    response.json(result.rows[0])
+  }
+})
+
 db.query(`
   CREATE TABLE IF NOT EXISTS votes( 
     id SERIAL PRIMARY KEY,
