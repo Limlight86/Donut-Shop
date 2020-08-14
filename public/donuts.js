f235f430912d0ad68cabb162a03d0f4cab6d00b2
Lazaro <lquin008@gmail.com>
2020-08-14 15:54:30 -0400

    post request on front end added for buttons

--- public/donuts.js
+++ public/donuts.js
@@ -2,21 +2,48 @@ const main = document.querySelector('main')
 
 let donuts = []
 
+if(!localStorage.getItem("voterId")){
+  localStorage.setItem("voterId", String(Math.random()))
+}
+
+const voterId = localStorage.getItem("voterId")
+
+const voteForDonut = async (event) => {
+  const theButtonThatGotClicked = event.currentTarget
+  const theClosestDonut = theButtonThatGotClicked.closest(".donut")
+  const donutId = theClosestDonut.dataset.donut
+  const url = `/votes`;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({ voter:voterId, donut:donutId }),
+  });
+}
+
 const renderDonut = (donut) => {
-  return`
+  const html =`
     <div class="donut" data-donut=${donut.id}>      
       <img src=${donut.image} alt=${donut.name} />
       <h2 class="vote-count">0</h2>
       <button>${donut.name}</button>
     </div>
   `
+  const fragment = document.createRange().createContextualFragment(html) 
+  const button = fragment.querySelector("button")
+  button.addEventListener("click", voteForDonut)
+  return fragment
 }
 
 const fetchDonuts = async () => {
   const response = await fetch("https://donut-of-the-day.herokuapp.com/donuts")
   const result = await response.json()
   donuts = result
-  main.innerHTML = donuts.map(renderDonut).join("")
+  const fragments =  donuts.map(renderDonut)
+  main.innerHTML= ""
+  main.prepend(...fragments)
 }
 
 fetchDonuts()
